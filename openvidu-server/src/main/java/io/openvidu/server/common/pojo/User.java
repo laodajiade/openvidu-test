e3a0b8af13e51fca18e0f1144b8072d529c23ebd
zhangcunxi <zhangcunxi@suditech.cn>
2019-09-10 19:41:26 +0800

    添加自动生成的实体类、查询接口及mybatis查询xml

---
+++ openvidu-server/src/main/java/io/openvidu/server/common/pojo/User.java
@@ -0,0 +1,95 @@
+package io.openvidu.server.common.pojo;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private String uuid;
+
+    private String username;
+
+    private String phone;
+
+    private String email;
+
+    private String password;
+
+    private String project;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
