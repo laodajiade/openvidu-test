81a231e517075dedf8132c2299940f031d199612
zhangcunxi <zhangcunxi@suditech.cn>
2019-09-17 21:58:56 +0800

    缓存模板替换

--- openvidu-server/src/main/java/io/openvidu/server/common/cache/CacheManageImpl.java
+++ openvidu-server/src/main/java/io/openvidu/server/common/cache/CacheManageImpl.java
@@ -1,9 +1,12 @@
 package io.openvidu.server.common.cache;
 
 import io.openvidu.server.common.Contants.CacheKeyConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.Objects;
 
@@ -11,16 +14,19 @@ import java.util.Objects;
  * @author chosongi
  * @date 2019/9/12 14:50
  */
-
+@Slf4j
 @Component
 public class CacheManageImpl implements CacheManage {
 
-    @Resource(name = "tokenRedisTemplate")
-    private RedisTemplate<Object, Object> tokenRedisTemplate;
+    @Resource(name = "tokenStringTemplate")
+    private StringRedisTemplate tokenStringTemplate;
 
 
     @Override
     public boolean accessTokenEverValid(String userId, String token) {
-        return Objects.equals(tokenRedisTemplate.opsForHash().entries(CacheKeyConstants.APP_TOKEN_PREFIX_KEY + userId).get("token").toString(), token);
+        return Objects.equals(tokenStringTemplate.opsForHash().entries(CacheKeyConstants.APP_TOKEN_PREFIX_KEY + userId).get("token").toString(), token);
     }
+
+
+
 }
