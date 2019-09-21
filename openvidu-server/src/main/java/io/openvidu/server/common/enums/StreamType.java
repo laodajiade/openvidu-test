ffe7853a2fc5584bce8c6b2a5b30132b34cd77a7
zhangcunxi <zhangcunxi@suditech.cn>
2019-09-21 10:59:43 +0800

    定义码流类型

---
+++ openvidu-server/src/main/java/io/openvidu/server/common/enums/StreamType.java
@@ -0,0 +1,14 @@
+package io.openvidu.server.common.enums;
+
+/**
+ * @author chosongi
+ * @date 2019/9/20 18:21
+ */
+public enum StreamType {
+    /** 主码流*/
+    major,
+    /** 辅码流*/
+    minor,
+    /** 分享码流*/
+    sharing
+}
