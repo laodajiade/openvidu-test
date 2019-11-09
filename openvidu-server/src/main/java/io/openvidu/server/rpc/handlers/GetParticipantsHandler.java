cf3f3ebdb172bdabd72aac2a88e96c8384f0a504
yushaobo <yushaobo@sudi.corp>
2019-11-09 18:40:26 +0800

    获取参会人员列表

--- openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/GetParticipantsHandler.java
+++ openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/GetParticipantsHandler.java
@@ -94,7 +94,7 @@ public class GetParticipantsHandler extends RpcAbstractHandler {
 
                     userObj.addProperty("deviceName", device.getDeviceName());
                     userObj.addProperty("deviceOrgName", devDep.getDeptName());
-                    userObj.addProperty("appShowName", device.getDeviceName());
+                    userObj.addProperty("appShowName",  user.getUsername());
                     userObj.addProperty("appShowDesc", "(" + device.getDeviceModel() + ") " + devDep.getDeptName());
                 } else {
                     userObj.addProperty("appShowName", user.getUsername());
