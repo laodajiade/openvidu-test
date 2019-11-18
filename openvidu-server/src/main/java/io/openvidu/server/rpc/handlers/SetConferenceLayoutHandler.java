5f029641b5849f011c838024f9bca54d588bba75
zhangcunxi <zhangcunxi@suditech.cn>
2019-11-18 18:09:38 +0800

    布局发生变化通知kms

--- openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/SetConferenceLayoutHandler.java
+++ openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/SetConferenceLayoutHandler.java
@@ -1,17 +1,23 @@
 package io.openvidu.server.rpc.handlers;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import io.openvidu.client.internal.ProtocolElements;
 import io.openvidu.java.client.OpenViduRole;
 import io.openvidu.server.common.enums.ErrorCodeEnum;
 import io.openvidu.server.common.enums.LayoutModeEnum;
 import io.openvidu.server.common.layout.LayoutInitHandler;
+import io.openvidu.server.kurento.core.KurentoParticipant;
+import io.openvidu.server.kurento.core.KurentoSession;
 import io.openvidu.server.rpc.RpcAbstractHandler;
 import io.openvidu.server.rpc.RpcConnection;
 import lombok.extern.slf4j.Slf4j;
+import org.kurento.client.KurentoClient;
 import org.kurento.jsonrpc.message.Request;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Objects;
 
 @Slf4j
@@ -39,8 +45,42 @@ public class SetConferenceLayoutHandler extends RpcAbstractHandler {
                 return;
             }
 
+            boolean layoutModeChanged = Objects.equals(layoutModeEnum, conferenceSession.getLayoutMode());
+            JsonArray layouts = LayoutInitHandler.getLayoutByMode(layoutModeEnum);
             conferenceSession.setLayoutMode(layoutModeEnum);
-            conferenceSession.setLayoutCoordinates(LayoutInitHandler.getLayoutByMode(layoutModeEnum));
+            conferenceSession.setLayoutCoordinates(layouts);
+
+            // json RPC notify KMS layout changed.
+            if (layoutModeChanged) {
+                KurentoSession kurentoSession = (KurentoSession) conferenceSession;
+                KurentoClient kurentoClient = kurentoSession.getKms().getKurentoClient();
+                Request<JsonObject> kmsRequest = new Request<>();
+                JsonObject params = new JsonObject();
+                params.addProperty("object", kurentoSession.getPipeline().getId());
+                params.addProperty("operation", "setLayout");
+                JsonArray layoutInfos = new JsonArray(50);
+                for (JsonElement jsonElement : layouts) {
+                    JsonObject temp = jsonElement.getAsJsonObject();
+                    JsonObject resultPart = temp.deepCopy();
+                    KurentoParticipant kurentoParticipant = (KurentoParticipant) conferenceSession.getParticipantByPublicId(temp.get("connectionId").getAsString());
+                    resultPart.addProperty("object", kurentoParticipant.getPublisher().getWebEndpoint().getId());
+                    resultPart.addProperty("hasVideo", kurentoParticipant.getPublisherMediaOptions().hasVideo());
+                    resultPart.addProperty("onlineStatus", String.valueOf(cacheManage.getUserInfoByUUID(rpcConnection.getUserUuid()).get("status")));
+
+                    layoutInfos.add(resultPart);
+                }
+                params.add("layoutInfo", layoutInfos);
+                kmsRequest.setMethod("invoke");
+                kmsRequest.setParams(params);
+                try {
+                    kurentoClient.sendJsonRpcRequest(kmsRequest);
+                } catch (IOException e) {
+                    log.error("Exception:\n", e);
+                    this.notificationService.sendErrorResponseWithDesc(rpcConnection.getParticipantPrivateId(), request.getId(),
+                            null, ErrorCodeEnum.SERVER_INTERNAL_ERROR);
+                    return;
+                }
+            }
 
             // broadcast the changes of layout
             JsonObject notifyResult = new JsonObject();
