5b7939d7b3aa609f3dd17daf286a22c35b3e4ba8
zhangcunxi <zhangcunxi@suditech.cn>
2019-12-11 14:54:28 +0800

    去除候选地址接口兼容

--- openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/OnIceCandidateHandler.java
+++ openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/OnIceCandidateHandler.java
@@ -3,14 +3,11 @@ package io.openvidu.server.rpc.handlers;
 import com.google.gson.JsonObject;
 import io.openvidu.client.OpenViduException;
 import io.openvidu.client.internal.ProtocolElements;
-import io.openvidu.server.common.enums.StreamModeEnum;
 import io.openvidu.server.core.Participant;
 import io.openvidu.server.rpc.RpcAbstractHandler;
 import io.openvidu.server.rpc.RpcConnection;
 import org.kurento.jsonrpc.message.Request;
 import org.springframework.stereotype.Service;
-
-import java.util.Objects;
 
 /**
  * @author geedow
@@ -26,11 +23,6 @@ public class OnIceCandidateHandler extends RpcAbstractHandler {
         String candidate = getStringParam(request, ProtocolElements.ONICECANDIDATE_CANDIDATE_PARAM);
         String sdpMid = getStringParam(request, ProtocolElements.ONICECANDIDATE_SDPMIDPARAM);
         int sdpMLineIndex = getIntParam(request, ProtocolElements.ONICECANDIDATE_SDPMLINEINDEX_PARAM);
-        String streamMode = getStringOptionalParam(request, ProtocolElements.ONICECANDIDATE_STREAMMODE_PARAM);
-        if (!Objects.isNull(streamMode) &&
-                Objects.equals(StreamModeEnum.SFU_SHARING, StreamModeEnum.valueOf(streamMode))) {
-            endpointName = endpointName.substring(0, endpointName.indexOf("_"));
-        }
         try {
             participant = sanityCheckOfSession(rpcConnection, endpointName, "onIceCandidate");
         } catch (OpenViduException e) {
