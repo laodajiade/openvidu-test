6eebbdf4e756c058843d9df5f6e55b82b0e199e0
zhangcunxi <zhangcunxi@suditech.cn>
2019-12-26 20:40:36 +0800

    校验shareCastPlayStrategy合法性

--- openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/DistributeShareCastPlayStrategyHandler.java
+++ openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/DistributeShareCastPlayStrategyHandler.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import io.openvidu.client.internal.ProtocolElements;
 import io.openvidu.java.client.OpenViduRole;
 import io.openvidu.server.common.enums.ErrorCodeEnum;
+import io.openvidu.server.common.enums.ShareCastPlayStrategy;
 import io.openvidu.server.common.enums.StreamType;
 import io.openvidu.server.core.Participant;
 import io.openvidu.server.rpc.RpcAbstractHandler;
@@ -31,6 +32,7 @@ public class DistributeShareCastPlayStrategyHandler extends RpcAbstractHandler {
                     null, ErrorCodeEnum.REQUEST_PARAMS_ERROR);
             return;
         }
+        ShareCastPlayStrategy.valueOf(shareCastPlayStrategy);
 
         if (!OpenViduRole.MODERATOR_ROLES.contains(sessionManager.getParticipant(rpcConnection.getSessionId(),
                         rpcConnection.getParticipantPrivateId(), StreamType.MAJOR).getRole())) {
