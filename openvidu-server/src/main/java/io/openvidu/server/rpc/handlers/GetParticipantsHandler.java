b94cc71c7c778c46a42d050569b4d2d54c487ca2
张存曦 <zhangcunxi@suditech.cn>
2020-04-24 22:51:21 +0800

    与会者列表按混叠顺序返回

--- openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/GetParticipantsHandler.java
+++ openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/GetParticipantsHandler.java
@@ -1,13 +1,16 @@
 package io.openvidu.server.rpc.handlers;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import io.openvidu.client.internal.ProtocolElements;
 import io.openvidu.java.client.OpenViduRole;
 import io.openvidu.server.common.enums.AccessTypeEnum;
+import io.openvidu.server.common.enums.ConferenceModeEnum;
 import io.openvidu.server.common.enums.StreamType;
 import io.openvidu.server.common.enums.UserOnlineStatusEnum;
 import io.openvidu.server.common.pojo.*;
+import io.openvidu.server.core.Session;
 import io.openvidu.server.kurento.core.KurentoParticipant;
 import io.openvidu.server.rpc.RpcAbstractHandler;
 import io.openvidu.server.rpc.RpcConnection;
@@ -57,11 +60,11 @@ public class GetParticipantsHandler extends RpcAbstractHandler {
 
         if (!CollectionUtils.isEmpty(userIds)) {
             List<User> userList = userMapper.selectByPrimaryKeys(userIds);
-            userList.forEach(user -> {
+            for (User user : userList) {
                 KurentoParticipant part = (KurentoParticipant) sessionManager.getParticipants(sessionId).stream().filter(s -> user.getId()
                         .compareTo(Long.valueOf(s.getUserId())) == 0 && Objects.equals(StreamType.MAJOR, s.getStreamType()) &&
                         !Objects.equals(OpenViduRole.THOR, s.getRole())).findFirst().orElse(null);
-                if (Objects.isNull(part)) return;
+                if (Objects.isNull(part)) continue;
 
                 // User and dept info.
                 UserDeptSearch udSearch = new UserDeptSearch();
@@ -70,13 +73,14 @@ public class GetParticipantsHandler extends RpcAbstractHandler {
                 if (Objects.isNull(userDeptCom)) {
                     log.warn("GetParticipant userDept is null and privateId:{}, userId:{}",
                             part.getParticipantPrivateId(), user.getId());
-                    return;
+                    continue;
                 }
                 Department userDep = depMapper.selectByPrimaryKey(userDeptCom.getDeptId());
 
                 JsonObject userObj = new JsonObject();
                 userObj.addProperty("userId", user.getId());
                 userObj.addProperty("account", user.getUuid());
+                userObj.addProperty("connectionId", part.getParticipantPublicId());
                 userObj.addProperty("username", user.getUsername());
                 userObj.addProperty("userOrgName", userDep.getDeptName());
                 userObj.addProperty("role", part.getRole().name());
@@ -103,7 +107,7 @@ public class GetParticipantsHandler extends RpcAbstractHandler {
 
                     userObj.addProperty("deviceName", device.getDeviceName());
                     userObj.addProperty("deviceOrgName", devDep.getDeptName());
-                    userObj.addProperty("appShowName",  device.getDeviceName());
+                    userObj.addProperty("appShowName", device.getDeviceName());
                     userObj.addProperty("appShowDesc", "(" + device.getDeviceModel() + ") " + devDep.getDeptName());
                 } else {
                     userObj.addProperty("appShowName", user.getUsername());
@@ -111,8 +115,39 @@ public class GetParticipantsHandler extends RpcAbstractHandler {
                 }
 
                 jsonArray.add(userObj);
-            });
+            }
         }
+
+        // return the composite order according to session majorShareMixLinkedArr and exclude sharing
+        Session session = sessionManager.getSession(sessionId);
+        if (ConferenceModeEnum.MCU.equals(session.getConferenceMode())) {
+            JsonArray majorShareMixLinkedArr = sessionManager.getSession(sessionId).getMajorShareMixLinkedArr();
+            JsonArray orderPartArr = new JsonArray(64);
+            JsonArray disorderPartArr = new JsonArray(50);
+            List<String> list = new ArrayList<>(16);
+            for (JsonElement jsonElement : majorShareMixLinkedArr) {
+                JsonObject partObj = jsonElement.getAsJsonObject();
+                list.add(partObj.get("connectionId").getAsString());
+                for (JsonElement je : jsonArray) {
+                    String partConnectId = je.getAsJsonObject().get("connectionId").getAsString();
+                    if (partObj.get("connectionId").getAsString().equals(partConnectId)) {
+                        orderPartArr.add(je);
+                        break;
+                    }
+                }
+            }
+
+            for (JsonElement je : jsonArray) {
+                String partConnectId = je.getAsJsonObject().get("connectionId").getAsString();
+                if (!list.contains(partConnectId)) {
+                    disorderPartArr.add(je);
+                }
+            }
+
+            orderPartArr.addAll(disorderPartArr);
+            jsonArray = orderPartArr;
+        }
+
         respJson.add("participantList", jsonArray);
         notificationService.sendResponse(rpcConnection.getParticipantPrivateId(), request.getId(), respJson);
     }
