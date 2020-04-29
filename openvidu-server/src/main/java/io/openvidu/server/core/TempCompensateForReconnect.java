8f4d447fc2abc5794df34f0321229cb1d84fa54a
张存曦 <zhangcunxi@suditech.cn>
2020-04-29 22:16:18 +0800

    主持人断线未重连的进行过期剔出（90s），关闭会议

--- openvidu-server/src/main/java/io/openvidu/server/core/TempCompensateForReconnect.java
+++ openvidu-server/src/main/java/io/openvidu/server/core/TempCompensateForReconnect.java
@@ -1,10 +1,14 @@
 package io.openvidu.server.core;
 
+import com.google.gson.JsonObject;
 import io.openvidu.client.internal.ProtocolElements;
 import io.openvidu.java.client.OpenViduRole;
+import io.openvidu.server.common.cache.CacheManage;
 import io.openvidu.server.common.enums.ConferenceModeEnum;
+import io.openvidu.server.common.enums.DeviceStatus;
 import io.openvidu.server.common.enums.ParticipantHandStatus;
 import io.openvidu.server.common.enums.StreamType;
+import io.openvidu.server.rpc.RpcConnection;
 import io.openvidu.server.rpc.RpcNotificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.TaskScheduler;
@@ -33,7 +37,10 @@ public class TempCompensateForReconnect {
     @Resource(name = "reconnectCompensationScheduler")
     private TaskScheduler taskScheduler;
 
-    public static final int EVICT_PART_NOT_RECONNECT_DURATION = 90;
+    @Resource
+    private CacheManage cacheManage;
+
+    private static final int EVICT_PART_NOT_RECONNECT_DURATION = 90;
 
     public void addReconnectCheck(Session session, Participant participant) {
         UnReconnectPartEvictHandler unReconnectPartEvictHandler = new UnReconnectPartEvictHandler(session.getSessionId(),
@@ -69,38 +76,54 @@ public class TempCompensateForReconnect {
                     Participant participant;
                     if (Objects.nonNull(participant = session.getParticipantByPublicId(partPublicId))
                             && partCreateTime.compareTo(participant.getCreatedAt()) == 0) {
-                        String speakerId = null, moderatorId = null;
-                        Set<Participant> participants = sessionManager.getParticipants(sessionId);
-                        for (Participant part : participants) {
-                            if (StreamType.MAJOR.equals(part.getStreamType())) {
-                                if (part.getRole().equals(OpenViduRole.MODERATOR)) {
-                                    moderatorId = part.getParticipantPublicId();
-                                }
-                                if (Objects.equals(ParticipantHandStatus.speaker, part.getHandStatus())) {
-                                    speakerId = part.getParticipantPublicId();
+                        if (!OpenViduRole.MODERATOR.equals(participant.getRole())) {
+                            String speakerId = null, moderatorId = null;
+                            Set<Participant> participants = sessionManager.getParticipants(sessionId);
+                            for (Participant part : participants) {
+                                if (StreamType.MAJOR.equals(part.getStreamType())) {
+                                    if (part.getRole().equals(OpenViduRole.MODERATOR)) {
+                                        moderatorId = part.getParticipantPublicId();
+                                    }
+                                    if (Objects.equals(ParticipantHandStatus.speaker, part.getHandStatus())) {
+                                        speakerId = part.getParticipantPublicId();
+                                    }
                                 }
                             }
-                        }
 
-                        log.info("Before CompensateForReconnect majorShareMixLinkedArr:{}",
-                                session.getMajorShareMixLinkedArr().toString());
-                        session.leaveRoomSetLayout(participant, !Objects.equals(speakerId, participant.getParticipantPublicId()) ?
-                                speakerId : moderatorId);
+                            log.info("Before CompensateForReconnect majorShareMixLinkedArr:{}",
+                                    session.getMajorShareMixLinkedArr().toString());
+                            session.leaveRoomSetLayout(participant, !Objects.equals(speakerId, participant.getParticipantPublicId()) ?
+                                    speakerId : moderatorId);
 
-                        log.info("After CompensateForReconnect majorShareMixLinkedArr:{}",
-                                session.getMajorShareMixLinkedArr().toString());
-                        session.invokeKmsConferenceLayout();
+                            log.info("After CompensateForReconnect majorShareMixLinkedArr:{}",
+                                    session.getMajorShareMixLinkedArr().toString());
+                            session.invokeKmsConferenceLayout();
 
-                        log.info("CompensateForReconnect evict part:{}", partPublicId);
-                        sessionManager.evictParticipant(participant, null, null,
-                                EndReason.sessionClosedByServer);
+                            log.info("CompensateForReconnect evict part:{}", partPublicId);
+                            sessionManager.evictParticipant(participant, null, null,
+                                    EndReason.sessionClosedByServer);
 
-                        for (Participant participant1 : participants) {
-                            if (Objects.equals(StreamType.MAJOR, participant1.getStreamType())
-                                    && !Objects.equals(participant, participant1)) {
-                                rpcNotificationService.sendNotification(participant1.getParticipantPrivateId(),
-                                        ProtocolElements.CONFERENCELAYOUTCHANGED_NOTIFY, session.getLayoutNotifyInfo());
+                            for (Participant participant1 : participants) {
+                                if (Objects.equals(StreamType.MAJOR, participant1.getStreamType())
+                                        && !Objects.equals(participant, participant1)) {
+                                    rpcNotificationService.sendNotification(participant1.getParticipantPrivateId(),
+                                            ProtocolElements.CONFERENCELAYOUTCHANGED_NOTIFY, session.getLayoutNotifyInfo());
+                                }
                             }
+
+                        } else {
+                            sessionManager.getSession(sessionId).getParticipants().forEach(p -> {
+                                if (!Objects.equals(StreamType.MAJOR, p.getStreamType())) {
+                                    return;
+                                }
+                                rpcNotificationService.sendNotification(p.getParticipantPrivateId(), ProtocolElements.CLOSE_ROOM_NOTIFY_METHOD, new JsonObject());
+                                RpcConnection rpcConnect = rpcNotificationService.getRpcConnection(p.getParticipantPrivateId());
+                                if (!Objects.isNull(rpcConnect) && !Objects.isNull(rpcConnect.getSerialNumber())) {
+                                    cacheManage.setDeviceStatus(rpcConnect.getSerialNumber(), DeviceStatus.online.name());
+                                }});
+
+                            sessionManager.updateConferenceInfo(sessionId);
+                            sessionManager.closeSession(sessionId, EndReason.closeSessionByModerator);
                         }
 
                         log.info("CompensateForReconnect clean up the unused rpc connection:{}", partPrivateId);
@@ -112,7 +135,7 @@ public class TempCompensateForReconnect {
             }
         };
 
-        public void evictIfNecessary() {
+        void evictIfNecessary() {
             evictTask = taskScheduler.schedule(evictThread,
                     new Date(System.currentTimeMillis() + EVICT_PART_NOT_RECONNECT_DURATION * 1000 ));
         }
