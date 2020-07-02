da350c48df3f14ae9e555b281f47ac268a1481d8
张存曦 <zhangcunxi@suditech.cn>
2020-07-02 20:01:35 +0800

    删除无用sql

--- openvidu-server/src/main/java/io/openvidu/server/common/dao/CorpMcuConfigMapper.java
+++ openvidu-server/src/main/java/io/openvidu/server/common/dao/CorpMcuConfigMapper.java
@@ -3,20 +3,5 @@ package io.openvidu.server.common.dao;
 import io.openvidu.server.common.pojo.CorpMcuConfig;
 
 public interface CorpMcuConfigMapper {
-
-    int deleteByPrimaryKey(Long id);
-
-    int insert(CorpMcuConfig record);
-
-    int insertSelective(CorpMcuConfig record);
-
-    CorpMcuConfig selectByPrimaryKey(Long id);
-
-    int updateByPrimaryKeySelective(CorpMcuConfig record);
-
-    int updateByPrimaryKey(CorpMcuConfig record);
-
     CorpMcuConfig selectByProject(String project);
-
-    void updateByProject(CorpMcuConfig corpMcuConfig);
 }
