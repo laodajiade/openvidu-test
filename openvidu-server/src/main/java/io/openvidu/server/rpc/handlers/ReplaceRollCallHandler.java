b1526e7a1fe71d3612015e70a08b385024f05e97
张存曦 <zhangcunxi@suditech.cn>
2020-08-06 16:01:51 +0800

    替换发言接口、通知修改

--- openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/ReplaceRollCallHandler.java
+++ openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/ReplaceRollCallHandler.java
@@ -10,7 +10,6 @@ import io.openvidu.server.rpc.RpcConnection;
 import org.kurento.jsonrpc.message.Request;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -21,37 +20,23 @@ import java.util.Set;
 public class ReplaceRollCallHandler extends RpcAbstractHandler {
     @Override
     public void handRpcRequest(RpcConnection rpcConnection, Request<JsonObject> request) {
-        // TODO, Fixme. Maybe have good way to do it. for example: add a roll call type.
         String sessionId = getStringParam(request, ProtocolElements.REPLACE_ROLL_CALL_ROOM_ID_PARAM);
-        String sourceId = getStringParam(request, ProtocolElements.REPLACE_ROLL_CALL_SOURCE_ID_PARAM);
         String endTargetId = getStringParam(request, ProtocolElements.REPLACE_ROLL_CALL_END_TARGET_ID_PARAM);
         String startTargetId = getStringParam(request, ProtocolElements.REPLACE_ROLL_CALL_START_TARGET_ID_PARAM);
 
         Set<Participant> participants = sessionManager.getParticipants(sessionId);
-        participants.stream().filter(part ->
-                endTargetId.equals(gson.fromJson(part.getClientMetadata(), JsonObject.class).get("clientData").getAsString()))
-                .findFirst().get().setHandStatus(ParticipantHandStatus.down);
-
-        participants.stream().filter(part ->
-                startTargetId.equals(gson.fromJson(part.getClientMetadata(), JsonObject.class).get("clientData").getAsString()))
-                .findFirst().get().setHandStatus(ParticipantHandStatus.speaker);
-
-        int raiseHandNum = 0;
-        for (Participant p : sessionManager.getParticipants(sessionId)) {
-            if (p.getHandStatus() == ParticipantHandStatus.up) {
-                ++raiseHandNum;
-            }
-        }
-
-        JsonObject params = new JsonObject();
-        params.addProperty(ProtocolElements.REPLACE_ROLL_CALL_ROOM_ID_PARAM, sessionId);
-        params.addProperty(ProtocolElements.REPLACE_ROLL_CALL_SOURCE_ID_PARAM, sourceId);
-        params.addProperty(ProtocolElements.REPLACE_ROLL_CALL_END_TARGET_ID_PARAM, endTargetId);
-        params.addProperty(ProtocolElements.REPLACE_ROLL_CALL_START_TARGET_ID_PARAM, startTargetId);
-        params.addProperty(ProtocolElements.REPLACE_ROLL_CALL_RAISEHAND_NUMBER_PARAM, raiseHandNum);
         participants.forEach(participant -> {
-            if (!Objects.equals(StreamType.MAJOR, participant.getStreamType())) return;
-            this.notificationService.sendNotification(participant.getParticipantPrivateId(), ProtocolElements.REPLACE_ROLL_CALL_METHOD, params);
+            if (StreamType.MAJOR.equals(participant.getStreamType())) {
+                if (endTargetId.equals(participant.getUuid())) {
+                    participant.setHandStatus(ParticipantHandStatus.down);
+                }
+
+                if (startTargetId.equals(participant.getUuid())) {
+                    participant.setHandStatus(ParticipantHandStatus.speaker);
+                }
+                this.notificationService.sendNotification(participant.getParticipantPrivateId(),
+                        ProtocolElements.REPLACE_ROLL_CALL_METHOD, request.getParams());
+            }
         });
 
         this.notificationService.sendResponse(rpcConnection.getParticipantPrivateId(), request.getId(), new JsonObject());
