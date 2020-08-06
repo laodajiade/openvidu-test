804531e7ff2cf6db25efa8c79e3f357beeaf62dd
张存曦 <zhangcunxi@suditech.cn>
2020-08-06 15:55:09 +0800

    与会者放手接口优化处理

--- openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/PutDownHandHandler.java
+++ openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/PutDownHandHandler.java
@@ -4,9 +4,6 @@ import com.google.gson.JsonObject;
 import io.openvidu.client.internal.ProtocolElements;
 import io.openvidu.server.common.enums.ParticipantHandStatus;
 import io.openvidu.server.common.enums.StreamType;
-import io.openvidu.server.common.pojo.Device;
-import io.openvidu.server.common.pojo.DeviceSearch;
-import io.openvidu.server.common.pojo.User;
 import io.openvidu.server.core.Participant;
 import io.openvidu.server.rpc.RpcAbstractHandler;
 import io.openvidu.server.rpc.RpcConnection;
@@ -27,53 +24,22 @@ import java.util.Set;
 public class PutDownHandHandler extends RpcAbstractHandler {
     @Override
     public void handRpcRequest(RpcConnection rpcConnection, Request<JsonObject> request) {
+        String targetId;
         String sessionId = getStringParam(request, ProtocolElements.PUT_DOWN_HAND_ROOM_ID_PARAM);
-        String sourceId = getStringParam(request, ProtocolElements.PUT_DOWN_HAND_SOURCE_ID_PARAM);
-        String targetId = request.getParams().has(ProtocolElements.PUT_DOWN_HAND_TARGET_ID_PARAM) ?
-                request.getParams().get(ProtocolElements.PUT_DOWN_HAND_TARGET_ID_PARAM).getAsString() : null;
+        boolean isControlAll = StringUtils.isEmpty(targetId = getStringOptionalParam(request,
+                ProtocolElements.PUT_DOWN_HAND_TARGET_ID_PARAM));
         Set<Participant> participants = sessionManager.getParticipants(sessionId);
-        if (!StringUtils.isEmpty(targetId)) {
-            Participant raisePart = sessionManager.getParticipants(sessionId).stream().filter(participant -> Objects.equals(targetId,
-                    participant.getUserId()) && Objects.equals(StreamType.MAJOR, participant.getStreamType()))
-                    .findFirst().orElse(null);
-            if (!Objects.isNull(raisePart)) {
-                raisePart.setHandStatus(ParticipantHandStatus.down);
-            } else return;
-        } else {
-            participants.forEach(part -> part.setHandStatus(ParticipantHandStatus.down));
-        }
-
-        User user = userMapper.selectByPrimaryKey(Long.valueOf(sourceId));
-        String	serialNumber = rpcConnection.getSerialNumber();
-        if (!Objects.isNull(serialNumber))
-            log.info("serialNumber", serialNumber);
-        // device info.
-        Device device=null;
-        if (!StringUtils.isEmpty(serialNumber)) {
-            DeviceSearch deviceSearch = new DeviceSearch();
-            deviceSearch.setSerialNumber(serialNumber);
-            device = deviceMapper.selectBySearchCondition(deviceSearch);
-        }
-        JsonObject params = new JsonObject();
-        params.addProperty(ProtocolElements.PUT_DOWN_HAND_ROOM_ID_PARAM, sessionId);
-        params.addProperty(ProtocolElements.PUT_DOWN_HAND_SOURCE_ID_PARAM, sourceId);
-        params.addProperty(ProtocolElements.PUT_DOWN_USERNAME_PARAM, user.getUsername());
-        if (!StringUtils.isEmpty(device))
-            params.addProperty(ProtocolElements.PUT_DOWN_APPSHOW_NAME_PARAM, device.getDeviceName());
-        if (!StringUtils.isEmpty(targetId)) {
-            params.addProperty(ProtocolElements.PUT_DOWN_HAND_TARGET_ID_PARAM, targetId);
-            int raiseHandNum = 0;
-            for (Participant p : sessionManager.getParticipants(sessionId)) {
-                if (Objects.equals(ParticipantHandStatus.up, p.getHandStatus()) &&
-                        Objects.equals(StreamType.MAJOR, p.getStreamType())) {
-                    ++raiseHandNum;
-                }
-            }
-            params.addProperty(ProtocolElements.PUT_DOWN_HAND_RAISEHAND_NUMBER_PARAM, raiseHandNum);
-        }
         participants.forEach(participant -> {
-            if (!Objects.equals(StreamType.MAJOR, participant.getStreamType())) return;
-            this.notificationService.sendNotification(participant.getParticipantPrivateId(), ProtocolElements.PUT_DOWN_HAND_METHOD, params);
+            if (Objects.equals(StreamType.MAJOR, participant.getStreamType())) {
+                if (!isControlAll && targetId.equals(participant.getUuid())) {
+                    participant.setHandStatus(ParticipantHandStatus.down);
+                } else {
+                    participant.setHandStatus(ParticipantHandStatus.down);
+                }
+
+                this.notificationService.sendNotification(participant.getParticipantPrivateId(),
+                        ProtocolElements.PUT_DOWN_HAND_METHOD, request.getParams());
+            }
         });
 
         this.notificationService.sendResponse(rpcConnection.getParticipantPrivateId(), request.getId(), new JsonObject());
