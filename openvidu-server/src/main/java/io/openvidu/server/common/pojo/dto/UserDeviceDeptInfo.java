3328dc94ecd25ab9a4ce61e0f919ce6240737638
张存曦 <zhangcunxi@suditech.cn>
2020-08-07 22:20:10 +0800

    dto类提交

---
+++ openvidu-server/src/main/java/io/openvidu/server/common/pojo/dto/UserDeviceDeptInfo.java
@@ -0,0 +1,18 @@
+package io.openvidu.server.common.pojo.dto;
+
+import lombok.Data;
+
+/**
+ * @author chosongi
+ * @date 2020/8/7 19:19
+ */
+@Data
+public class UserDeviceDeptInfo {
+    private Long userId;
+    private String uuid;
+    private String username;
+    private Long deptId;
+    private String deptName;
+    private String serialNumber;
+    private String deviceName;
+}
