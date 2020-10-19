dc8fe764f8adb56e29df2ca047f3b2978b2a70a9
yangyu <Sudikeji0826>
2020-10-19 19:13:27 +0800

    会议日程

--- openvidu-server/src/main/java/io/openvidu/server/common/dao/AppointConferenceMapper.java
+++ openvidu-server/src/main/java/io/openvidu/server/common/dao/AppointConferenceMapper.java
@@ -36,7 +36,7 @@ public interface AppointConferenceMapper {
 
     List<AppointConference> getConflictAppointConferenceList(AppointConference condition);
 
-    List<AppointConference> pendingAboutAppointment(Long userId);
+    List<AppointConference> pendingAboutAppointment(AppointConference appointConference);
 
     void changeStatusByRuid(@Param("status") int status, @Param("ruid") String ruid);
 }
--- openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/GetConferenceScheduleHandler.java
+++ openvidu-server/src/main/java/io/openvidu/server/rpc/handlers/GetConferenceScheduleHandler.java
@@ -12,16 +12,23 @@ import io.openvidu.server.core.PageResult;
 import io.openvidu.server.core.RespResult;
 import io.openvidu.server.domain.vo.ConferenceHisResp;
 import io.openvidu.server.domain.vo.GetConferenceScheduleVO;
+import io.openvidu.server.exception.BindValidateException;
 import io.openvidu.server.rpc.ExRpcAbstractHandler;
 import io.openvidu.server.rpc.RpcConnection;
 import io.openvidu.server.utils.BindValidate;
+import io.openvidu.server.utils.DateUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.checkerframework.checker.units.qual.A;
 import org.kurento.jsonrpc.message.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -100,8 +107,21 @@ public class GetConferenceScheduleHandler extends ExRpcAbstractHandler<GetConfer
         }
 
         private List<ConferenceHisResp> pendingAboutAppointment() {
+            AppointConference appointConference = new AppointConference();
+            appointConference.setUserId(userId);
 
-            List<AppointConference> appointConferenceList = appointConferenceMapper.pendingAboutAppointment(userId);
+            if (StringUtils.isNotBlank(vo.getDate())) {
+                try {
+                    appointConference.setStartTime(DateUtils.parseDate(vo.getDate()+" 00:00:00","yyyy-MM-dd HH:mm:ss"));
+                    appointConference.setEndTime(DateUtils.parseDate(vo.getDate()+" 23:59:59","yyyy-MM-dd HH:mm:ss"));
+                } catch (ParseException e) {
+                    log.error("date parse error error",e);
+                    throw new BindValidateException("date parse error date=" + vo.getDate());
+                }
+            }
+
+
+            List<AppointConference> appointConferenceList = appointConferenceMapper.pendingAboutAppointment(appointConference);
 
 
             return transferResp(appointConferenceList);
