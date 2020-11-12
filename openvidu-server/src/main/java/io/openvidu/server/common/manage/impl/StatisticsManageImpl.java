40dd42dc6332e7fd03789e3ea53a77aaabb15aff
qin.bin <qin.bin@trs.com.cn>
2020-11-12 21:02:15 +0800

    修改计算进行会议消耗时长

--- openvidu-server/src/main/java/io/openvidu/server/common/manage/impl/StatisticsManageImpl.java
+++ openvidu-server/src/main/java/io/openvidu/server/common/manage/impl/StatisticsManageImpl.java
@@ -11,11 +11,11 @@ import io.openvidu.server.common.pojo.Conference;
 import io.openvidu.server.common.pojo.ConferencePartHistory;
 import io.openvidu.server.common.pojo.Corporation;
 import io.openvidu.server.common.pojo.StatisticsConferenceDaily;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,6 +25,7 @@ import java.util.Objects;
  * @author chosongi
  * @date 2020/7/22 15:15
  */
+@Slf4j
 @Service
 public class StatisticsManageImpl implements StatisticsManage {
 
@@ -75,13 +76,12 @@ public class StatisticsManageImpl implements StatisticsManage {
             List<ConferencePartHistory> conferencePartHistories = conferencePartHistoryMapper.selectProcessPartHistory(project);
             if (!CollectionUtils.isEmpty(conferencePartHistories)) {
                 int totalUsedDuration = 0;
-                Date currentDate = new Date();
                 for (ConferencePartHistory conferencePartHistory : conferencePartHistories) {
-                    int usedDuration = (int) (currentDate.getTime() - conferencePartHistory.getStartTime().getTime())/60000;
-                    totalUsedDuration += usedDuration;
+                    totalUsedDuration ++;
                 }
                 int remainderTotalDuration = remainderDuration - totalUsedDuration;
                 corporation.setRemainderDuration(remainderTotalDuration);
+                log.info("在会议中与会者耗时-totalUsedDuration:{},剩余时长-remainderDuration:{}",totalUsedDuration,remainderDuration);
                 corporationMapper.updateCorpRemainderDuration(corporation);
                 int remainderHour = remainderTotalDuration/60;
                 int remainderMinute = remainderTotalDuration%60;
