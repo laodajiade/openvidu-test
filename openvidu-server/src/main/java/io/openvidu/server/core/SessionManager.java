86664baa3566e589a2969b6689bb2a7da84e0fa0
qin.bin <qin.bin@trs.com.cn>
2020-12-10 19:13:52 +0800

    屏幕入会允许推流

--- openvidu-server/src/main/java/io/openvidu/server/core/SessionManager.java
+++ openvidu-server/src/main/java/io/openvidu/server/core/SessionManager.java
@@ -456,7 +456,7 @@ public abstract class SessionManager {
 
 				return sessionPresetEnum.equals(SessionPresetEnum.on) ? (OpenViduRole.PUBLISHER.equals(participant.getRole())
 						|| OpenViduRole.MODERATOR.equals(participant.getRole()) || OpenViduRole.SUBSCRIBER.equals(participant.getRole())) : (OpenViduRole.PUBLISHER.equals(participant.getRole())
-						|| OpenViduRole.MODERATOR.equals(participant.getRole()));
+						|| OpenViduRole.MODERATOR.equals(participant.getRole()) || OpenViduRole.ONLY_SHARE.equals(participant.getRole()));
 			} else {
 				return false;
 			}
