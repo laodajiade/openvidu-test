ee5dcafbaa4507b6d09dacc19f33442b61dcc570
qin.bin <qin.bin@trs.com.cn>
2020-12-24 11:12:34 +0800

    修复web会控调整与会者顺序NPE

--- openvidu-server/src/main/java/io/openvidu/server/core/Session.java
+++ openvidu-server/src/main/java/io/openvidu/server/core/Session.java
@@ -818,7 +818,7 @@ public class Session implements SessionInterface {
 					int oldOrder = participant.getOrder();
 					Integer newOrder = partOrderMap.get(participant.getUuid());
 					log.info("web drag participant:{},oldOrder:{},newOrder:{}",participant.getUuid(),oldOrder,newOrder);
-					if (!Objects.equals(oldOrder, newOrder)) {
+					if (Objects.nonNull(newOrder) && !Objects.equals(oldOrder, newOrder)) {
 						participant.setOrder(newOrder);
 						if (Math.max(oldOrder, newOrder) >= lineOrder && lineOrder >= Math.min(oldOrder, newOrder)
 								&& !OpenViduRole.MODERATOR.equals(participant.getRole())) {  // exclude the moderator
