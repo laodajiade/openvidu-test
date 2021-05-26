27a795f6d220eb21153324c317d4a0e0e1586672
liqianfeng <liqianfeng@suditech.cn>
2021-05-26 10:58:07 +0800

    隐藏手机号

--- openvidu-server/src/main/java/io/openvidu/server/common/manage/HiddenPhoneManage.java
+++ openvidu-server/src/main/java/io/openvidu/server/common/manage/HiddenPhoneManage.java
@@ -2,6 +2,7 @@ package io.openvidu.server.common.manage;
 
 import io.openvidu.server.common.pojo.AllUserInfo;
 import io.openvidu.server.common.pojo.UserGroupVo;
+import io.openvidu.server.common.pojo.vo.OftenContactsVo;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface HiddenPhoneManage {
     void hiddenPhone(List<UserGroupVo> userGroups);
 
     void hiddenPhone2(List<AllUserInfo> allUserInfos);
+
+    void hiddenContactsPhone(List<OftenContactsVo> oftenContactsVos);
 }
